0961e19a995f6e898a8a82ffbc59531151892c2d
Samuel Carvalho <carvalho.asamuel@gmail.com>
2018-09-27 21:08:59 -0300

    Generating Token for validation

--- app/src/main/java/carvalho/android/com/br/zapzap/Activity/LoginActivity.java
+++ app/src/main/java/carvalho/android/com/br/zapzap/Activity/LoginActivity.java
@@ -2,16 +2,22 @@ package carvalho.android.com.br.zapzap.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.github.rtoshiro.util.format.SimpleMaskFormatter;
 import com.github.rtoshiro.util.format.text.MaskTextWatcher;
 
+import java.util.Random;
+
 import carvalho.android.com.br.zapzap.R;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText telefone, ddd, pais;
+    private EditText telefone, ddd, pais, nome;
+    private Button cadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,8 @@ public class LoginActivity extends AppCompatActivity {
         telefone = (EditText)findViewById(R.id.editTextTelefone);
         ddd = (EditText)findViewById(R.id.editTextDDD);
         pais = (EditText)findViewById(R.id.editTextPais);
+        nome = (EditText) findViewById(R.id.editTextNome);
+        cadastrar = (Button)findViewById(R.id.buttonCadastro);
 
         // criando as mascaras
         SimpleMaskFormatter smfPais = new SimpleMaskFormatter("+NN");
@@ -36,5 +44,23 @@ public class LoginActivity extends AppCompatActivity {
         pais.addTextChangedListener(maskPais);
         ddd.addTextChangedListener(maskDDD);
         telefone.addTextChangedListener(maskTelefone);
+
+        cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String nomeUsuario = nome.getText().toString();
+                String telefoneCompleto = pais.getText().toString() + ddd.getText().toString() + telefone.getText().toString();
+                String telefoneSemFormatacao = telefoneCompleto.replace("+", "");
+                telefoneSemFormatacao = telefoneSemFormatacao.replace("-","");
+
+                //Gerar token
+                Random random = new Random();
+                int numeroRandomico = random.nextInt(9999-1000)+1000;
+
+                String token = String.valueOf(numeroRandomico);
+
+            }
+        });
     }
 }
